b29618304b665411616aae1c5f42a0a1d5a4c9b0
Kutbettin Kizilkaya <kutbettin.kizilkaya@semine.no>
2021-05-04 10:59:38 +0200

    Add project files.

---
+++ Composition/Logger.cs
@@ -0,0 +1,12 @@
+﻿namespace Composition
+{
+    public class Logger
+    {
+
+
+        public void Log(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+}
